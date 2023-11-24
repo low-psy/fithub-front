44e59de467a79c9deeef1c352ef205dde12b0c6c
PSY <sksssd234@naver.com>
2023-11-24 16:19:47 +0900

    chore: tailwind theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#9766FF',
+        sub: '#E0D1FF',
+        input_bg: '#ECEEEF ',
+        accent: '#FFA5A5',
+        accent_sub: '#FFECEC',
+      },
+      screens: {
+        start: '0px',
+      },
+      minWidth: {
+        400: '400px',
+      },
+    },
   },
   plugins: [],
 };
