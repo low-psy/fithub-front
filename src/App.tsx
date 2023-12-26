d00f535c25b95e9ca1a7250a2b8c0bc520071457
Minggurazi <jhsong1450@naver.com>
2023-12-26 22:08:47 +0900

    feat: 라우팅 경로 추가 및 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import FindPassword from './pages/help/password';
 import Home from './pages/home';
 import Login from './pages/login';
 import LoginRedirect from './pages/login/LoginRedirect';
+import NotFound from './pages/NotFound';
 import Signup from './pages/signup';
+import AdditionalInfo from './pages/signup/AdditionalInfo';
+import CertifyTrainer from './pages/certifyTrainer';
+import EmailAuthentication from './pages/signup/EmailAuthentication';
+import SignupSuccess from './pages/signup/SignupSuccess';
+import SocialSignup from './pages/signup/SocialSignup';
 
 function App() {
   return (
@@ -13,10 +19,22 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/login/redirect/*" element={<LoginRedirect />} />
+          <Route path="/signup" element={<Signup />}>
+            <Route path="email" element={<EmailAuthentication />} />
+            <Route path="additional-info" element={<AdditionalInfo />} />
+            <Route path="success" element={<SignupSuccess />} />
+            <Route path="certify-trainer" element={<CertifyTrainer />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+          <Route path="/oauth2/regist" element={<SocialSignup />} />
+
+          <Route
+            path="/oauth2/authorization/google"
+            element={<LoginRedirect />}
+          />
           <Route path="/help/password" element={<FindPassword />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
