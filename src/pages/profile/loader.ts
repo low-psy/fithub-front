c05eb63d6186f8cb9b328085b94ac7368fcd15ea
Minggurazi <jhsong1450@naver.com>
2024-01-22 20:57:52 +0900

    remove: 기존 profile 컴포넌트 삭제

--- src/pages/profile/loader.ts
+++
@@ -1,8 +0,0 @@
-import { authAxios } from '../../apis/axios';
-
-const loader = async () => {
-  const response = await authAxios.get('/users/profile');
-  return response.data;
-};
-
-export default loader;
