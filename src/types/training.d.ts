b373b668b126c07f2360088ab5e2255507d94a28
PSY <sksssd234@naver.com>
2024-01-25 19:59:08 +0900

    chore: swagger type 추가 및 최신 업데이트

--- src/types/training.d.ts
+++ src/types/training.d.ts
@@ -1,10 +1,7 @@
 import { LoaderFunction } from 'react-router-dom';
 
-export type LoaderData<TLoaderFn extends LoaderFunction> = Awaited<
-  ReturnType<TLoaderFn>
-> extends Response | infer D
-  ? D
-  : never;
+export type LoaderData<TLoaderFn extends LoaderFunction> =
+  Awaited<ReturnType<TLoaderFn>> extends Response | infer D ? D : never;
 
 export interface TrainerData {
   id: number;
--- src/types/trainingClass.ts
+++ src/types/trainingClass.ts
@@ -1,3 +1,4 @@
+import { TrainingInfoDto } from './swagger/model/trainingInfoDto';
 import { TrainerData, TrainingDto } from './training';
 
 export default class Training implements TrainingDto {
@@ -54,7 +55,6 @@ export default class Training implements TrainingDto {
 }
 
 export function createFakeData(): Training {
-  console.log('execute');
   const trainers: TrainerData[] = Array.from({ length: 10 }, (_, index) => ({
     id: index,
     trainerInfoDto: {
@@ -70,8 +70,6 @@ export function createFakeData(): Training {
     endDate: '2022-12-31',
     closed: false,
   }));
-
-  console.log(trainers);
 
   return new Training({
     totalPages: 10,
@@ -101,4 +99,60 @@ export function createFakeData(): Training {
     last: false,
     empty: false,
   });
+}
+
+export function createDetailFakeData(): TrainingInfoDto {
+  const training: TrainingInfoDto = {
+    id: 0,
+    trainerInfoDto: {
+      trainerId: 0,
+      name: `Trainer ${0}`,
+      trainerProfileImg: `http://hitgym.tcubemnet.com/static/datas/board/attach/images/2019/05/20/722eae27d1c80499d453e72c68f052a1.jpg`,
+      location: '대한민국 서울특별시 중구 을지로3가, 305',
+    },
+    title: `Title ${0}`,
+    content: `content ${0}`,
+    images: [
+      {
+        id: 0,
+        url: 'https://www.100yearshop.co.kr/shop/data/editor/47bab845854505cc.png',
+        inputName: 'inputname',
+      },
+      {
+        id: 0,
+        url: 'https://www.100yearshop.co.kr/shop/data/editor/47bab845854505cc.png',
+        inputName: 'inputname',
+      },
+      {
+        id: 0,
+        url: 'https://www.100yearshop.co.kr/shop/data/editor/47bab845854505cc.png',
+        inputName: 'inputname',
+      },
+    ],
+    quota: 10,
+    price: 100000,
+    location: '대한민국 서울특별시 중구 을지로3가',
+    startDate: '2024-01-19',
+    endDate: '2024-01-19',
+    availableDates: [
+      {
+        id: 0,
+        date: '2024-01-19',
+        enabled: true,
+        availableTimes: [
+          {
+            id: 0,
+            time: {
+              hour: 12,
+              minute: 0,
+              second: 0,
+              nano: 0,
+            },
+            enabled: true,
+          },
+        ],
+      },
+    ],
+  };
+  return training;
 }
