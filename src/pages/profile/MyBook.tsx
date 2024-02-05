95ed13c6b8b124952dd5630b9771ae0dfcc8811d
Minggurazi <jhsong1450@naver.com>
2024-02-06 00:07:44 +0900

    remove: 중복 컴포넌트 제거

--- src/pages/profile/MyBook.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyBook = () => {
-  return <div>MyBook</div>;
-};
-
-export default MyBook;
--- src/pages/profile/MyCancel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyCancel = () => {
-  return <div>MyCancel</div>;
-};
-
-export default MyCancel;
