70045fb841f1def609a5a97069c2e28631ef7c28
Minggurazi <jhsong1450@naver.com>
2024-02-05 21:07:36 +0900

    feat: 프로필 페이지 라우팅 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -41,20 +41,13 @@ import Profile from './pages/user/profile';
 import Posts from './pages/user/posts';
 import Reservations from './pages/user/Reservation';
 import Cancellation from './pages/user/cancellation';
-import EditProfile from './pages/user/profile/EditProfile';
+import EditProfile from './pages/user/profile/editProfile/EditProfile';
 
 import profileLoader from './pages/user/loader';
 
-import withAuth from './hocs/withAuth';
 import Help from './pages/help';
 
 function App() {
-  const AuthedCertifyTrainer = withAuth(
-    CertifyTrainer,
-    true,
-    '/certify-trainer',
-  );
-
   // useEffect(() => {
   //   loadGoogleMapsAPI();
   // }, []);
@@ -83,6 +76,8 @@ function App() {
         { path: 'post', element: <Post />, loader: postLoader },
         // 소셜 회원가입
         { path: 'oauth2/regist', element: <SocialSignup /> },
+        // 트레이너 인증
+        { path: 'certify-trainer', element: <CertifyTrainer /> },
         // 유저 프로필
         {
           path: 'user',
@@ -90,7 +85,7 @@ function App() {
           loader: profileLoader,
           children: [
             {
-              index: true,
+              path: 'profile',
               element: <Profile />,
             },
             {
@@ -114,15 +109,13 @@ function App() {
         },
       ],
     },
-    // 비밀번호 찾기
-    // { path: 'help/password', element: <FindPassword /> },
+    // 비밀번호 찾기 (임시 비밀번호 발급)
     {
       path: '/help',
       element: <Help />,
       children: [{ path: 'password', element: <FindPassword /> }],
     },
-    // 트레이너 인증
-    { path: 'certify-trainer', element: <AuthedCertifyTrainer /> },
+
     // 로그인
     {
       path: '/login',
